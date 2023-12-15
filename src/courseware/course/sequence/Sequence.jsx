d7016c7654ecc5782f474dc626fbfa10b75c4eaf
Calvin Jackson <amigohmccclurkin98@gmail.com>
2023-12-15 18:06:00 +0300

    wip

--- src/courseware/course/sequence/Sequence.jsx
+++ src/courseware/course/sequence/Sequence.jsx
@@ -149,7 +149,7 @@ function Sequence({
   };
 
   const defaultContent = (
-    <div className="sequence-container d-inline-flex flex-row">
+    <div className="sequence-container w-100 d-inline-flex flex-row">
       <div className={classNames('sequence w-100', { 'position-relative': shouldDisplayNotificationTriggerInSequence })}>
         <SequenceNavigation
           sequenceId={sequenceId}
